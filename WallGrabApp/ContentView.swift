82062f393256c69026115d6f9cd36271bc59794c
jvcleave <jvcleave@gmail.com>
2026-04-07 23:30:21 -0400

    Initial Commit

---
+++ WallGrabApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WallGrabApp
+//
+//  Created by jason van cleave on 4/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WallGrabApp/WallGrabAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallGrabAppApp.swift
+//  WallGrabApp
+//
+//  Created by jason van cleave on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct WallGrabAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
